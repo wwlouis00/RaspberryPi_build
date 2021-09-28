d3962309a1eaa68a42c5335f198207673e986ec0
wwlouis00 <64067917+wwlouis00@users.noreply.github.com>
2021-09-28 14:25:05 +0800

    Update update.sh

--- 腳本/update.sh
+++ 腳本/update.sh
@@ -6,3 +6,5 @@ sudo apt-get autoremove python2.7
 sudo rm /usr/bin/python
 sudo rm /usr/bin/pip
 sudo apt-get install python3.7
+sudo ln -s /usr/bin/python3.7 /usr/bin/python
+sudo ln -s /usr/bin/pip3 /usr/bin/pip
