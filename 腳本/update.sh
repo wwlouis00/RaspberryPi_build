f37b0ad2952facf2cbfc59bd53e841e6f789adcd
wwlouis00 <64067917+wwlouis00@users.noreply.github.com>
2021-10-13 16:21:06 +0800

    Update update.sh

--- 腳本/update.sh
+++ 腳本/update.sh
@@ -20,4 +20,8 @@ sudo apt-get autoremove
 
 # Install Picamera
 sudo apt-get install python-picamera python3-picamera
-
+sudo apt-get install build-essential cmake pkg-config
+sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install libxvidcore-dev libx264-dev
+sudo apt-get install libgtk2.0-dev
