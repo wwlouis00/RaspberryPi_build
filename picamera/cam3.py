20858c6ffc5b71b0ab9a4cc0ceeb9b4a53d18fb8
wwlouis00 <64067917+wwlouis00@users.noreply.github.com>
2021-10-13 16:05:54 +0800

    Update cam3.py

--- picamera/cam3.py
+++ picamera/cam3.py
@@ -1,1 +1,13 @@
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+camera.rotation = 0
+
+camera.start_preview()
+camera.start_recording('/home/pi/Louis/picamera/video.h264')
+sleep(5)
+camera.stop_recording()
+camera.stop_preview()
+
 
