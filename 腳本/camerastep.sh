d3baba4cb702d8604ea4f0329ec0675ba2de9a89
unknown <wwlouis00@gmail.com>
2022-01-27 18:11:36 +0800

    remove somefiles

--- 腳本/camerastep.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get install -y libhdf5-dev libhdf5-serial-dev libatlas-base-dev libjasper-dev libqtgui4 libqt4-test 
-
-pip install opencv-python==4.0.1.24
-pip install xlwt
-pip install xlrd
-pip install xlutils
-pip install pyzbar
-sudo apt-get install python3-pyqt5
